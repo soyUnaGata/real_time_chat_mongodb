ff59f453e88b176c9407778baba3f1fbb34bc707
soyUnaGata <fallen.snitch@gmail.com>
2023-10-16 12:00:23 +0300

    amend api comments

--- client_v2/index.js
+++ client_v2/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000; // You can change the port as needed
+const port = 3000; 
 const srcDirname = __dirname + '/src'
 app.use('/static', express.static(srcDirname + '/static'));
 
-// Define routes
+
 app.get('/login', (req, res) => {
   res.sendFile(srcDirname + '/login.html');
 });
@@ -17,7 +17,7 @@ app.get('/chat', (req, res) => {
   res.sendFile(srcDirname + '/chat.html');
 });
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
